a3a9564173da19deaabab00983ce33e7212e900e
Rarelo <rarelo@rarelo.net>
2022-06-08 17:17:14 -0700

    added automatic firefox profiles on linux

---
+++ create-profile.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#echo "profile_name: $1";
+#echo "$1";
+
+#go to home directory
+cd
+home="$(pwd)"
+#echo $home
+
+#save home directory as var $home
+cd $home/.mozilla/firefox #go to
+#firefox="$(pwd)" #save firefox directory
+
+#save vars for default folder and destination new_profile folder
+default="$(find . -maxdepth 1 -name "*default*" -print)"
+new_profile="$(find . -maxdepth 1 -name "*$1*" -print)"
+
+echo $default
+echo $new_profile
+
+cp -r $default/* $new_profile
